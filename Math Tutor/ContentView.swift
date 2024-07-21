93997e38d8935646c4ac59938a29c4430eb08031
Donglinnn <65762101+Donglinnn@users.noreply.github.com>
2024-07-21 20:33:08 +0800

    Initial Commit

---
+++ Math Tutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Math Tutor
+//
+//  Created by Che-lun Hu on 2024/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by Che-lun Hu on 2024/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
